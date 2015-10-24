0546290bf5b76e0691e387a4df91ef2cb926c56d
joseph-galindo <joseph.galindo18@gmail.com>
2015-10-24 03:41:43 -0400

    TODO: scrap all these animations since hiding content behind mouseovers is a horrible idea (re: mobile/tablet)

--- js/animations.js
+++ js/animations.js
@@ -13,38 +13,54 @@ $(document).ready(function() {
 	var t3 = $('#title3');
 	var t4 = $('#title4');
 
-	var c1 = $('#content1');
-	var c2 = $('#content2');
-	var c3 = $('#content3');
-	var c4 = $('#content4');
-
-	//Text vars
-
+	//Timer vars
 	var seconds = 1;
+
+	//Text objects
+
+	var titles = {
+		'1': 'Code',
+		'2': 'CoTN',
+		'3': 'Boats',
+		'4': 'Music'
+	};
+
+	var content = {
+		'1': 'Here is a lot of cool text about code.',
+		'2': 'Here is some text about a meme game.',
+		'3': '2D boats are really cool.',
+		'4': 'I listen to way too much music.'
+	};
 
 	//FUNCTIONS
 
-	var fadeoutTitle = function(title, content) {
+	var fadeIntoContent = function(title, idx) {
 
 		console.log('hovered over container');
 
-		TweenMax.to(title,1,{autoAlpha:0});
-		TweenMax.to(content,1,{autoAlpha:1});
+		var thecontent = content[idx];
+
+		setTimeout(function() { title.html(thecontent)},(seconds*1000));
+		TweenMax.to(title,seconds,{autoAlpha:0});
+		TweenMax.to(title,seconds,{autoAlpha:1, delay:seconds});
 	};
 
-	var fadeoutContent = function(title, content) {
+	var fadeIntoTitle = function(title, idx) {
 
 		console.log('leave container');
 
-		TweenMax.to(title,1,{autoAlpha:1});
-		TweenMax.to(content,1,{autoAlpha:0});
+		var thetitle = titles[idx];
+
+		setTimeout(function() { title.html(thetitle)},(seconds*1000));
+		TweenMax.to(title,seconds,{autoAlpha:0});
+		TweenMax.to(title,seconds,{autoAlpha:1, delay:seconds});
 	};
 
 	//INIT
 
-	first.hover(function() {fadeoutTitle(t1,c1)}, function() {fadeoutContent(t1,c1)});
-	second.hover(function() {fadeoutTitle(t2,c2)},function() {fadeoutContent(t2,c2)}); 
-	third.hover(function() {fadeoutTitle(t3,c3)},function() {fadeoutContent(t3,c3)}); 
-	fourth.hover(function() {fadeoutTitle(t4,c4)},function() {fadeoutContent(t4,c4)});  
+	first.hover(function() {fadeIntoContent(t1,'1')}, function() {fadeIntoTitle(t1,'1')});
+	second.hover(function() {fadeIntoContent(t2,'2')}, function() {fadeIntoTitle(t2,'2')}); 
+	third.hover(function() {fadeIntoContent(t3,'3')}, function() {fadeIntoTitle(t3,'3')}); 
+	fourth.hover(function() {fadeIntoContent(t4,'4')}, function() {fadeIntoTitle(t4,'4')});  
 
 });
