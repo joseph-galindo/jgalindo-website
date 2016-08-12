e2c966d57d71ca47c76a2024f7782d8d5cf037f3
Joseph Galindo <joseph.galindo18@gmail.com>
2016-08-12 00:55:14 -0400

    revert to non-bootstrap, work on layout, header, and index page a bit

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,58 @@
-$(document).ready(function() {
-	
-	// do js onload memes
+$(function() {
 
 	console.log('document ready from app.js');
 
-});
+	//declare functions
 
-// do js memes that need to live outside onready scope
+	function checkHeaderPos() {
+		var currentTop = $(window).scrollTop();
+		var headerHeight = $('.first-row').height();
+		var isFixed = $('.header').hasClass('fixed');
+
+		if(currentTop >= headerHeight && !isFixed) {
+			//if scrolled past the height of the header
+			//and the header has not been fixed yet
+			//fix the header
+			$('.header').addClass('fixed');
+		}
+
+		if(currentTop < headerHeight && isFixed) {
+			//if scrolled before the height of the header
+			//and the header is fixed from prior scrolling
+			//unfix the header
+			$('.header').removeClass('fixed');
+		}
+	}
+
+	function checkNav() {
+	    var nav = $('.header')
+	    var anchor = nav.find('a');
+	    var current = window.location.pathname.split('/')[1];
+	    // current = current.replace('.html','');
+
+	    for (var i = 0; i < anchor.length; i++) {
+
+	    	//this split is messy...doublecheck this works as intended in prod environment
+
+	        if((anchor[i].href).split('/')[3] == current) {
+	            anchor[i].className = "active";
+	        }
+
+	        if(current == '') {
+	        	anchor[0].className = "active";
+	        }
+	    }
+	}
+
+	//declare listeners
+
+	$(window).scroll(function(event) {
+		console.log('scroll');
+		checkHeaderPos();
+	});
+
+	//arbitrary executions go here
+
+	checkNav();
+
+});
